24c78611920a7e5914fd0386f3b53aa81876b620
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-09-05 23:45:49 +0100

    added the result column

--- models/history.py
+++ models/history.py
@@ -3,14 +3,15 @@ User History.
 """
 from typing import Any
 from models.base import Base, BaseModel
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, Text
 from sqlalchemy.orm import relationship
 
 class UserHistory(BaseModel, Base):
     __tablename__ = 'history'
     action = Column("Action", String(128))
-    query = Column("Query", String(128))
+    query = Column("Query", Text)
     user_id = Column("user_id", String(128), ForeignKey('user.id'))
+    result = Column("Result", Text)
     
     # Establish the relationship with User
     user = relationship("User", back_populates="histories")
