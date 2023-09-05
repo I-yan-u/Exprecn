e8082202506bdc210a6b7ea3faee107f39a44b49
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-09-05 23:43:46 +0100

    added import for exprecn_api

--- api/v1/obj_views/__init__.py
+++ api/v1/obj_views/__init__.py
@@ -6,3 +6,4 @@ app_view = Blueprint('app_view', __name__, url_prefix='/api/v1')
 from api.v1.obj_views.index import *
 from api.v1.obj_views.user_api import *
 from api.v1.obj_views.history_api import *
+from api.v1.obj_views.exprecn_api import *
