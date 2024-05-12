add41e462063d49abcce9ffd5fcac81989546151
I-yan-u <iyanuajimobi12@gmail.com>
2024-05-12 21:48:22 +0100

    chore: Refactor user history API endpoints and add JWT authentication

--- api/v2/obj_views/history_api.py
+++ api/v2/obj_views/history_api.py
@@ -30,45 +30,53 @@ def user_history(user):
         user_hist.append(hist.to_dict())
     return jsonify(user_hist)
 
-@app_view.route('/user/history/id', methods=['GET'], strict_slashes=False)
+@app_view.route('/user/history/<string:id>', methods=['GET'], strict_slashes=False)
 @jAuth.token_required
-def specific_user_history(user):
+def specific_user_history(user, id):
     """Returns specific user"""
-    id = request.args.get('id')
     if not user:
         return make_response(jsonify({'error': 'Unauthorised'}), 401)
     spec_user_history = store.get_hist_user(user.id, id)
-    if spec_user_history is None:
+    if not spec_user_history:
         return make_response(jsonify({'error': 'User history not found'}), 404)
     return jsonify(spec_user_history.to_dict())
  
-@app_view.route('/users/<user_id>/history/<id>', methods=['DELETE'], strict_slashes=False)
-def del_user_history(user_id, id):
+@app_view.route('/user/history/<string:id>', methods=['DELETE'], strict_slashes=False)
+@jAuth.token_required
+def del_user_history(user, id):
     """Deletes a user information"""
-    specific_hist = store.get_hist_user(user_id, id)
-    if specific_hist is None:
-        abort(404)
+    if not user:
+        return make_response(jsonify({'error': 'Unauthorised'}), 401)
+    specific_hist = store.get_hist_user(user.id, id)
+    if not specific_hist:
+        return make_response(jsonify({'error': 'User history not found'}), 404)
     store.delete(specific_hist)
     store.save()
     remaining_hist = []
-    for hist in store.get_hist_user(user_id):
+    for hist in store.get_hist_user(user.id):
         remaining_hist.append(hist.to_dict())
     return jsonify(remaining_hist)
 
-@app_view.route('/users/<user_id>/history', methods=['DELETE'], strict_slashes=False)
-def del_all_user_history(user_id):
+@app_view.route('/user/history', methods=['DELETE'], strict_slashes=False)
+@jAuth.token_required
+def del_all_user_history(user):
     """Deletes a user information"""
-    user_history = store.get_hist_user(user_id)
+    if not user:
+        return make_response(jsonify({'error': 'Unauthorised'}), 401)
+    user_history = store.get_hist_user(user.id)
     if user_history is None:
-        abort(404)
+        return make_response(jsonify({'error': 'User history not found'}), 404)
     for hist in user_history:
         store.delete(hist)
     store.save()
     return jsonify({})
 
-@app_view.route('/users/history', methods=['POST'], strict_slashes=False)
-def create_user_history():
+@app_view.route('/user/history', methods=['POST'], strict_slashes=False)
+@jAuth.token_required
+def create_user_history(user):
     """Create a new user"""
+    if not user:
+        return make_response(jsonify({'error': 'Unauthorised'}), 401)
     data = request.get_json()
     if not data:
         return make_response(jsonify({"error": "Not a JSON"}), 400)
@@ -76,28 +84,25 @@ def create_user_history():
         return make_response(jsonify({"error": "Missing action"}), 400)
     if 'query' not in data:
         return make_response(jsonify({"error": "Missing Sequence"}), 400)
-    if 'user_id' not in data:
-        return make_response(jsonify({"error": "Missing User_id"}), 400)
-    
+    data.update({'user_id': user.id})
     new_user_history = UserHistory(**data)
-    store.new(new_user_history)
-    store.save()
+    new_user_history.save()
     return make_response(jsonify(new_user_history.to_dict()), 201)
 
-@app_view.route('/users/history/<string:id>', methods=['PUT'], strict_slashes=False)
-def update_user_history(id):
-    """ Update user information"""
-    specific_hist = store.get(UserHistory, id)
-    if specific_hist is None:
-        abort(404)
-    data = request.get_json()
-    if not data:
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
-    for k, v in data.items():
-        if k != 'id':
-            setattr(specific_hist, k, v)
-    store.save()
-    return make_response(jsonify(specific_hist.to_dict()), 201)
+# @app_view.route('/users/history/<string:id>', methods=['PUT'], strict_slashes=False)
+# def update_user_history(id):
+#     """ Update user information"""
+#     specific_hist = store.get(UserHistory, id)
+#     if specific_hist is None:
+#         abort(404)
+#     data = request.get_json()
+#     if not data:
+#         return make_response(jsonify({"error": "Not a JSON"}), 400)
+#     for k, v in data.items():
+#         if k != 'id':
+#             setattr(specific_hist, k, v)
+#     store.save()
+#     return make_response(jsonify(specific_hist.to_dict()), 201)
 
     
     
