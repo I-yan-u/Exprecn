50ff3f746d2360442812646c9417a5c70879ce61
I-yan-u <iyanuajimobi12@gmail.com>
2024-05-14 23:25:28 +0100

    chore: Register admin blueprint in Flask app

--- api/v2/app.py
+++ api/v2/app.py
@@ -4,6 +4,7 @@ API version 1.0.0
 """
 from flask import Flask, make_response, jsonify
 from api.v2.obj_views import app_view
+from api.v2.admin import admin
 from models import store
 from flask_cors import CORS
 from config import ENV
@@ -11,6 +12,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_view)
+app.register_blueprint(admin)
 CORS(app, supports_credentials=True, origins=["http://0.0.0.0:5000", "http://127.0.0.1:5000",
                                               "http://localhost:5000"])
 run_type = getenv('RUNSTAGE') or ENV['stage']
