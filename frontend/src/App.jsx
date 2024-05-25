6c3219661ea326e2db692e4acb1025d60dde723f
I-yan-u <iyanuajimobi12@gmail.com>
2024-05-26 00:41:16 +0100

    feat: Add routes and components for Home, Profile, and History pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
-//import { useState } from 'react'
+import {Routes, Route} from 'react-router-dom';
+import { Home, Profile, History } from './routes';
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/history' element={<History />} />
+    </Routes>
   )
 }
 
---
+++ frontend/src/routes/index.jsx
@@ -0,0 +1,5 @@
+import Home from "./Home";
+import Profile from "./Profile";
+import History from "./History";
+
+export { Home, Profile, History };
