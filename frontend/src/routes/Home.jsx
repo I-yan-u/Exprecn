86daea03c6581c1dbbc5466088a052518f364bf5
I-yan-u <iyanuajimobi12@gmail.com>
2024-05-26 17:42:40 +0100

    feat: Add Home page layout with header, footer, and content sections

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,8 +1,17 @@
 // import React from 'react'
+import style from './css/Home.module.css'
+import Dna1 from '../assets/logo.svg'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className={style.container} >
+        <div className={style.left}>
+            <h1>Exprecn</h1>
+        </div>
+        <div className={style.right}>
+            <img src={Dna1} alt="DNA1" />
+        </div>
+    </div>
   )
 }
 
