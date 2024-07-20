e93e1dbfe4a6fb087fa1df3b722c3c022a7885bd
I-yan-u <iyanuajimobi12@gmail.com>
2024-07-20 23:29:24 +0100

    refactor: Update Profile component with CSS styles, profile image, and form layout

--- frontend/src/routes/Profile.jsx
+++ frontend/src/routes/Profile.jsx
@@ -1,8 +1,26 @@
 // import React from 'react'
+import style from './css/Profile.module.css'
+import ProfileImage from '../assets/profile-circle.svg'
+import {Header, Footer} from '../layout'
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <>
+      <Header bg={true} />
+        <section className={style.profile}>
+          <div className={style.left}>
+            <img src={ProfileImage} alt="Profile image place holder" />
+            <p>Name</p>
+            <p>Descriptive bio</p>
+          </div>
+          <div className={style.right}>
+            <form>
+
+            </form>
+          </div>
+        </section>
+      <Footer />
+    </>
   )
 }
 
